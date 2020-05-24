ef512de56de67ccbb21612d505ed5167a8d48134
planetsugar <paulkeller@planetsugar.co.uk>
2020-05-24 15:46:03 +0100

    Added initial typescript-with-babel-boilerplate project

---
+++ typescript-with-babel-boilerplate/src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.PureComponent {
+	render() {
+		return (
+			<div>
+				<h1>Hello World - if you can see this then it is working.</h1>
+			</div>
+		);
+	}
+}
+
+export default App;
